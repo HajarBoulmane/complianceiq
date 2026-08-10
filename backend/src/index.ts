68cbd21bee79f93521f6bbc0372d90e2a87c6ce4
HajarBoulmane <hajarboulmane2003@gmail.com>
2026-08-11 00:24:29 +0100

    fix: prod start command + dynamic CORS origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,10 +15,10 @@
   const PORT = process.env.PORT || 3000;
 
 
-  app.use(cors({
-    origin: "http://localhost:5173", // ton frontend Vite
-    credentials: true,
-  }));
+ app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}));
   app.use(cookieParser());
   app.use(helmet());
   app.use(morgan("dev"));
